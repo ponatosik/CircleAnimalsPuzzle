1051864bcb8647c3d7dd49fbe66ee6a09f87daf5
ponatosik <ponatosik@gmail.com>
2023-09-26 12:46:59 +0300

    Add gameplay prototype

---
+++ Assets/Scripts/DestroyOnTouch.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DestroyOnTouch : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/FaceVelocityDirection.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FaceVelocityDirection : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate()
+    {
+        float angle = Vector2.SignedAngle(_rigidbody.velocity, Vector2.right);
+        transform.eulerAngles = new Vector3(0, 0, -angle);
+        if (Mathf.Abs(angle) > 90)
+        {
+            transform.localScale = new Vector3(1, -1, 1);
+        }
+        else 
+        {
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+	}
+}
