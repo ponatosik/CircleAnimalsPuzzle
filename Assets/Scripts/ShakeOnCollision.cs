7220a647e25f6267423bd27ec7a04ac992a21c5d
ponatosik <ponatosik@gmail.com>
2023-10-02 20:31:07 +0300

    Make mushrooms shake on collision

---
+++ Assets/Scripts/ShakeOnCollision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Animator))]
+
+public class ShakeOnCollision : MonoBehaviour
+{
+	Animator _animator;
+
+	private void Start()
+	{
+		_animator = GetComponent<Animator>();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		_animator.SetTrigger("Shake");
+	}
+}
