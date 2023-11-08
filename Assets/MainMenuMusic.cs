60ee80c7cd57530ec0defe1b9cf34180a8ee9421
Divam <masterpemoiseenko1@gmail.com>
2023-11-08 11:46:30 +0200

    Add MainMenu music

---
+++ Assets/MainMenuMusic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    void Start()
+    {
+        AudioManager.instance.Play("MainMenuMusic");
+    }
+}
