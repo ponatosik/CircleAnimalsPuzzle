a1452ec7c8bf7e707734f7fbb482149937665062
Divam <masterpemoiseenko1@gmail.com>
2023-12-05 22:36:46 +0200

    Fix stars render

--- Assets/Scripts/UI/StarsInLevel.cs
+++ Assets/Scripts/UI/StarsInLevel.cs
@@ -1,66 +1,41 @@
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
 
 public class Starsinlevel : MonoBehaviour
 {
+
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+
+    public static Starsinlevel instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     public static void OutputCollectedNumber()
     {
         int collectedNumber = GameManager.Instance.Collectables.GetCollectedNumber();
         Debug.Log($"Number of collectibles: {collectedNumber}");
+
+        ActivateStars(collectedNumber);
     }
-    /*
-    [SerializeField]
-    private GameObject yellowStarPrefab;  
-    private GameObject emptyStarPrefab;
 
-    public void DisplayCollectedNumber(int collectedNumber)
+    private static void ActivateStars(int collectedNumber)
     {
-        foreach (Transform child in transform)
+        if (collectedNumber >= 1)
         {
-            Destroy(child.gameObject);
+            instance.star1.SetActive(true);
         }
-
-        // Display stars based on the collectedNumber
-        for (int i = 0; i < 3; i++)
+        if (collectedNumber >= 2)
         {
-            GameObject star;
-
-            if (i < collectedNumber)
-            {
-                star = Instantiate(yellowStarPrefab, transform);
-            }
-            else
-            {
-                star = Instantiate(emptyStarPrefab, transform);
-            }
-
-            Image starImage = star.GetComponent<Image>();
-            RectTransform rectTransform = star.GetComponent<RectTransform>();
-
-            rectTransform.sizeDelta = new Vector2(300f, 289f) * 0.25f;
-
-            float xPos = 0f;
-            float yPos = 0f;
-
-            if (i == 0)
-            {
-                xPos = -70f;
-                yPos = -20f;
-            }
-            else if (i == 1)
-            {
-                xPos = 0f;
-                yPos = -70f;
-            }
-            else if (i == 2)
-            {
-                xPos = 70f;
-                yPos = -20f;
-            }
-
-            rectTransform.localPosition = new Vector3(xPos, yPos, 0f);
+            instance.star2.SetActive(true);
         }
-    }*/
+        if (collectedNumber >= 3)
+        {
+            instance.star3.SetActive(true);
+        }
+    }
+
 }
