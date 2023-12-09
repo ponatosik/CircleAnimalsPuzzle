b5fa67ce68e0ac74ddd6405033dfac30500ebc00
Divam <masterpemoiseenko1@gmail.com>
2023-12-09 10:28:47 +0200

    Add volume settings

--- Assets/Scripts/Sounds/MainMenuMusic.cs
+++ Assets/Scripts/Sounds/MainMenuMusic.cs
@@ -1,11 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MainMenuMusic : MonoBehaviour
 {
+    public Slider musicSlider;
+    public Slider soundSlider;
+
     void Start()
     {
+<<<<<<< Updated upstream
         AudioManager.instance.PlayMusic("MainMenuMusic", 1);
+=======
+        AudioManager.instance.PlayMusic("MainMenuMusic");
+
+        if (AudioManager.instance != null)
+        {
+            musicSlider.onValueChanged.AddListener(SetMusicVolume);
+            soundSlider.onValueChanged.AddListener(SetSoundVolume);
+        }
+        else
+        {
+            Debug.LogError("AudioManager not found!");
+        }
+    }
+
+    private void SetMusicVolume(float volume)
+    {
+        AudioManager.instance.SetMusicVolume(volume);
+    }
+
+    private void SetSoundVolume(float volume)
+    {
+        AudioManager.instance.SetSoundVolume(volume);
+>>>>>>> Stashed changes
     }
 }
