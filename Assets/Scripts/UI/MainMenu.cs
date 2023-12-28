ec44e5dce7f7a321abd2396790ac38c1f33f4a03
Divam <masterpemoiseenko1@gmail.com>
2023-12-28 23:13:35 +0200

    Add stars counter

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI StarsText;
+
     public void Quit_game()
     {
         Application.Quit();
@@ -13,9 +16,22 @@ public class MainMenu : MonoBehaviour
     {
         Level LastUnlcockedLevel = LevelSystem.Instance.GetLastUnlcockedLevel();
 
-        if(LastUnlcockedLevel != null)
+        if (LastUnlcockedLevel != null)
         {
             LevelSystem.Instance.LoadLevel(LastUnlcockedLevel);
         }
+    }
+
+    void Update()
+    {
+        show_stars();
+    }
+
+    public void show_stars()
+    {
+        int collectedStars = LevelSystem.Instance.TotalStarsCollected;
+        int totalStars = LevelSystem.Instance.TotalStars;
+
+        StarsText.text = collectedStars.ToString() + " / " + totalStars.ToString();
     }
 }
